18c4142041f185fab0ee1ef882200beffd4fc6d1
Rachid Sebbouh <rsebbouh@cegep-ste-foy.qc.ca>
2019-07-30 16:26:47 -0400

    Premier essai.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # ki-start
+
 Questionnaire pour mieux démarrer dans Kraland Interactif (www.kraland.org)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <p class="question">{{ currentQuestion.intitule }}</p>
+
+    <div>
+      <div v-for="answer in answers">
+        {{ answer.intitule }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import questions_json from './data/questions.json';
+
+export default {
+  name: 'app',
+
+  data() {
+    return {
+      questions: questions_json,
+      answers: [],
+      currentQuestion: false,
+      selectedAnswer: false
+    }
+  },
+
+  created() {
+    this.showQuestion(0);
+  },
+
+  methods: {
+    showQuestion(id) {
+      this.currentQuestion = this.questions.filter(q => q.id === id)[0];
+      this.answers = this.currentQuestion.reponses;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
