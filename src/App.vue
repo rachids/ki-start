64b035d4747cd8aacdabfbf76fe8a82d12f19d58
Rachid Zuco (PC - miniTour - Win7) <r.zuco@yahoo.fr>
2019-07-30 19:34:11 -0400

    Sélection de réponse + visuel

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <div id="app">
-    <p class="question">{{ currentQuestion.intitule }}</p>
+    <div class="results" v-if="results">
+      <div>
+        <p>Résultats:</p>
+      </div>
 
-    <div>
-      <div v-for="answer in answers">
-        {{ answer.intitule }}
+      <div class="carrieres">
+        <p>Nous te recommandons les carrières suivantes :</p>
+
+        <ul>
+            <li v-for="carriere in selectedAnswer.carrieres">{{ carriere }}</li>
+        </ul>
+      </div>
+
+      <div class="restart">
+        <button class="btn" @click="init">
+          Recommencer
+        </button>
+      </div>
+    </div>
+    <div v-else>
+      <div class="question">
+        <p>{{ currentQuestion.intitule }}</p>
+      </div>
+
+      <div class="reponses">
+        <div v-for="answer in answers" class="reponse" @click="answerQuestion(answer)">
+          {{ answer.intitule }}
+        </div>
       </div>
     </div>
   </div>
@@ -21,18 +44,45 @@ export default {
       questions: questions_json,
       answers: [],
       currentQuestion: false,
-      selectedAnswer: false
+      selectedAnswer: false,
+      results: false
     }
   },
 
   created() {
-    this.showQuestion(0);
+    // On démarre l'app avec la première question.
+    this.init();
   },
 
   methods: {
+    init() {
+      this.reset();
+      this.showQuestion(0);
+    },
+
     showQuestion(id) {
-      this.currentQuestion = this.questions.filter(q => q.id === id)[0];
+      this.currentQuestion = this.questions.filter(q => q.id === id)[0]; //peut-être un meilleur moyen de récupérer la question ?
       this.answers = this.currentQuestion.reponses;
+    },
+
+    answerQuestion(answer) {
+      this.selectedAnswer = answer;
+      if(this.selectedAnswer.hasOwnProperty('question_suivante')) {
+        this.showQuestion(this.selectedAnswer.question_suivante);
+      } else {
+        this.showResults();
+      }
+    },
+
+    showResults() {
+      this.results = true;
+    },
+
+    reset() {
+      this.answers = [];
+      this.currentQuestion = false;
+      this.selectedAnswer = false;
+      this.results = false;
     }
   }
 }
@@ -46,5 +96,50 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  height: 100%;
 }
+
+  .question {
+    display: flex;
+    align-items: center;
+    text-align: center;
+  }
+
+  .question p {
+    margin: auto;
+    font-size: xx-large;
+    padding-bottom: 6rem;
+  }
+
+  .reponses {
+    display: flex;
+    justify-content: space-around;
+  }
+
+  .reponse {
+    width: 400px;
+    height: 250px;
+    background-color: #171738;
+    color: #8ef9f3;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    transition: background-color 1s;
+    font-size: x-large;
+    border-radius: 25px;
+  }
+
+  .reponse:hover {
+    background-color: #db5461;
+    cursor: pointer;
+  }
+
+  .btn {
+    display: inline-block;
+    padding: 0.46em 1.6em;
+    border: 0.1em solid #8EF9F3;
+    background-color: #593C8F;
+    color: #8EF9F3;
+  }
 </style>
